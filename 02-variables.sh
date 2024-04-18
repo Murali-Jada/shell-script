de20a0d14686596e70b9bb544e63460c98485dde
Murali <muralijada24@gmail.com>
2024-04-18 13:36:12 +0530

    hello script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rahim::  Hello Murali, How are you?"
+echo "Murali:: Hi rahim, I am fine. How are you?"
+echo "Rahim :: I am fine too. How is your work?"
+echo "Murali:: Not bad. I am thinking to upgrade to Devops"
