d24590c38e50fb3afccde968fcdba0f28c11114f
Murali <muralijada24@gmail.com>
2024-04-18 14:33:49 +0530

    hello script

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-    NO1=$1
-    NO2=$2
+NO1=$1
+NO2=$2
 
-    SUM=$(($NO1+$NO2))
+SUM=$(($NO1+$NO2))
 
-    echo "total 0f $NO1 and $NO2 is: $SUM"
-    
+echo "Total of $NO1 and $NO2 is: $SUM"
